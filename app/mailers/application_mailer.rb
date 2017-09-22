c7acfeb6e808cf6b62e2eddaf6c7c73001457878
talhaQaisrani <talhajunaid65@gmail.com>
2017-09-22 15:57:04 +0500

    Add functions to calculate wk points and fielding poins in calculate_match_points

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def calculate_points_email
+    mail(to: ['talhajunaid65@gmail.com', 'ahmad@amblersaleem.com'], subject: 'Performance Points', body: "Performance Points calculated Successfully")
+  end
 end

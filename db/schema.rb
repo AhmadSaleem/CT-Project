8a199955026909410f5cc41793b6989b59056d21
Ahmad Saleem <ahmadsaleem93@gmail.com>
2017-09-25 16:46:03 +0500

    Add column points_earned to teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922045547) do
+ActiveRecord::Schema.define(version: 20170925102216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -173,6 +173,7 @@ ActiveRecord::Schema.define(version: 20170922045547) do
     t.string "team_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "points_earned", default: 0
     t.index ["tournament_id"], name: "index_teams_on_tournament_id"
     t.index ["user_id"], name: "index_teams_on_user_id"
   end

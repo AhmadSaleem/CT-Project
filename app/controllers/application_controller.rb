2d6f5058530ad20e73c5bf944fe931be1b0df693
Ahmad Saleem <ahmadsaleem93@gmail.com>
2017-09-28 11:07:02 +0500

    Override active_admin methods to login user as admin user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: :true
+
+  def authenticate_active_admin_user!
+    authenticate_user!
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def current_admin_user
+    return nil if user_signed_in? && !current_user.admin?
+    current_user
+  end
 end
